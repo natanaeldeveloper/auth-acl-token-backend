1e666caaf7dc419036e767c32624e6298362e4d5
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-24 16:31:31 -0300

    feat: adicionado relacionamento ao model Permission
    - roles: muitas permissões pertencem a muitos papeis

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_permissions');
+    }
 }
