ae36410fe40a47811464b741d08e087dafad031d
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-24 16:32:49 -0300

    feat: adicionado relacionamentos ao model Role
    - permissions: muitos papeis possuem muitas permissões
    - users: muitos papeis possuem muitos usuários

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_roles');
+    }
 }
