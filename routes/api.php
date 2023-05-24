082b1558f6866f6e742761230a38c9b11f8bc426
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-24 17:59:27 -0300

    feat: adicionado novas rotas de api
    - login
    - logout
    - roles/*
    - permissions/*

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group( function() {
+
+    Route::post('logout', [LoginController::class, 'logout']);
+
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('permissions', PermissionController::class);
 });
