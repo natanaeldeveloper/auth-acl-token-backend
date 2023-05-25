a5998eb5fc8d9926f18d221baaecde153eb961ae
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-25 17:44:29 -0300

    feat: middleware criado e incluído no Kernel
    - app\Http\Middleware\AddCustomParameter.php
    - Responsável por adicionar o parâmetro 'time' as requests

---
+++ app/Http/Middleware/AddCustomParameter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddCustomParameter
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $responseData = $response->getData();
+        $responseData->time = Carbon::now();
+        $response->setData($responseData);
+
+        return $response;
+    }
+}
