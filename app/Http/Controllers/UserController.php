b5e822cfe311067e8cebd4b25d88fb0aa8b4c09f
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-25 11:03:06 -0300

    feat: CRUD de usuários implementado
    - Cadastro
    - Listagem
    - Edição
    - Visualização
    - Remoção

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,15 +6,19 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $response = User::orderBy('name')->paginate(10);
+        $response['time'] = Carbon::now();
+
+        return response()->json($response);
     }
 
     /**
@@ -22,7 +26,14 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $user = User::create($request->all());
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.created.success'),
+            'data'      => $user,
+            'time'      => Carbon::now(),
+        ], 201);
     }
 
     /**
@@ -30,7 +41,10 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return response()->json([
+            'data' => $user,
+            'time' => Carbon::now(),
+        ]);
     }
 
     /**
@@ -38,7 +52,14 @@ class UserController extends Controller
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $user->update($request->all());
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.updated.success'),
+            'data'      => $user,
+            'time'      => Carbon::now(),
+        ]);
     }
 
     /**
@@ -46,6 +67,13 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.deleted.success'),
+            'id'        => $user->id,
+            'time'      => Carbon::now(),
+        ]);
     }
 }
