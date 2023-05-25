3f81c662a5c51bf3ce4ab0bd008fbbe97247bfaa
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-24 21:11:12 -0300

    feat: adicionado primeiras regras de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,42 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
+
     public function login(Request $request)
     {
+        $credentials = $this->validated($request->all());
+
+        if (Auth::attempt($credentials)) {
+            //...
+        }
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function validated($data)
+    {
+        $validator = Validator::make($data, [
+            'email' => 'required|email|max:255',
+            'password' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        return $validator->validated();
     }
 
     public function logout(Request $request)
