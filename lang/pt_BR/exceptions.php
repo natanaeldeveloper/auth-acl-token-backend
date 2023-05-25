b078d34db214c7f883e5679ccfd289eaef23bf53
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-25 10:53:16 -0300

    feat: adicionado arquivos de tradução
    - suporte de tradução pt-RB para exceptions
    - suporte de tradução pt-BR para messages

--- lang/pt_BR/exceptions.php
+++ lang/pt_BR/exceptions.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'validation_error' => 'Erro de validação',
-    'not_found_error' => 'Recurso não encontrado',
-    'authorization_error' => 'Acesso não autorizado',
-    'authentication_error' => 'Permissão negada',
-    'method_not_allowed_error' => 'Método HTTP não permitido',
-    'generic_error' => 'Erro inesperado'
+    'validation_error' => 'Identifique e corriga os campos que não atendem aos requisitos especificados.',
+    'not_found_error' => 'Recurso não encontrada.',
+    'authorization_error' => 'Acesso não autorizado.',
+    'authentication_error' => 'Autenticação necessária.',
+    'method_not_allowed_error' => 'Método HTTP não permitido.',
+    'internal_server_error' => 'Erro interno do servidor.',
 ];
---
+++ lang/pt_BR/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'created' => [
+        'success' => 'Recurso criado(a) com sucesso.',
+        'error' => 'Erro ao criar o recurso.',
+    ],
+
+    'updated' => [
+        'success' => 'Recurso atualizado(a) com sucesso.',
+        'error' => 'Erro ao atualizar o recurso.',
+    ],
+
+    'deleted' => [
+        'success' => 'Recurso removido(a) com sucesso.',
+        'error' => 'Erro ao remover o recurso.',
+    ],
+
+    'not_found' => 'O recurso não foi encontrado.',
+];
