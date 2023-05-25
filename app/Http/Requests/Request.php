b2ec8e27a2bf46652d71d0f5254ea596db4fc403
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-05-25 10:59:50 -0300

    refact: herança dos requests padronizada
    - Agora os Requests estendem da classe Request.
    Afim de facilitar a adição de regras futuras

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class Request extends FormRequest
+{
+    //...
+}
