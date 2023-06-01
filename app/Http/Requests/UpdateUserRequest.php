7e867ae4d9cc0bf972a7e331baaca6401ac3d8e9
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-06-01 09:14:31 -0300

    feat: permissão de escrita de usuário incluído no FormRequest

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -11,7 +11,7 @@ class UpdateUserRequest extends Request
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->instance()->user()->tokenCan('user:write');
     }
 
     /**
