84d1122b012cfe3cb47569ce858f83d6e25d365a
natanaeldeveloper <712020:55cb029b-7bd2-4758-8b1b-b71541144f89@bots.bitbucket.org>
2023-07-03 10:00:19 -0300

    feat: adicionado models da sprint 1
    - models das entidades: orgao, tipos de anexo e tipo de orgão

---
+++ app/Models/Orgao.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orgao extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orgaos';
+
+    protected $fillable = [
+        'tipo_orgao_id',
+        'orgao_id',
+        'nome',
+        'sigla',
+    ];
+
+    public function tipoOrgao()
+    {
+        return $this->belongsTo(TipoOrgao::class, 'tipo_orgao_id', 'id');
+    }
+
+    public function tiposAnexos()
+    {
+        return $this->belongsToMany(TipoAnexo::class, 'tipos_anexos_orgaos', 'orgao_id', 'tipo_anexo_id');
+    }
+}
---
+++ app/Models/TipoAnexo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoAnexo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tipos_anexos';
+
+    protected $fillable = [
+        'nome',
+        'modelo',
+        'cor'
+    ];
+
+    public function orgaos()
+    {
+        return $this->belongsToMany(Orgao::class, 'tipo_anexo_id', 'orgao_id');
+    }
+
+}
