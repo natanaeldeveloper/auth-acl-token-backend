a265f074dafbc996103160084e6ea05a0efd9985
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-07-06 23:35:05 -0300

    feat: filtro de retorno dos dados da api
    - adicionado arquivos de resource e collection das entidades do sistema
    - responsáveis pela filtragem de campos que são retornados nas rotas

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use GDebrauwer\Hateoas\Traits\HasLinks;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    use HasLinks;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            '_links' => $this->links(),
+        ];
+    }
+}
