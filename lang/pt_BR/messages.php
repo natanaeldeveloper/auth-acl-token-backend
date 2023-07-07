3a21d04282185618b808652720ef4b24e46c43dd
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-07-06 23:23:39 -0300

    feat: alterado mensagens de retorno HTTP
    - As mensagens de exceções HTTP foram movidas para o arquivo 'messages'
    - Adicionado novas mensagens para códigos de erro 400, 500 e 503

--- lang/pt_BR/messages.php
+++ lang/pt_BR/messages.php
@@ -16,5 +16,14 @@ return [
         'error' => 'Erro ao remover o recurso.',
     ],
 
-    'not_found' => 'O recurso não foi encontrado.',
+    "exceptions" => [
+        "400" => "A solicitação enviada é inválida.",
+        "401" => "A solicitação requer autenticação ou as credenciais fornecidas são inválidas.",
+        "403" => "O acesso ao recurso é proibido. Você não tem permissão para acessá-lo.",
+        "404" => "O recurso solicitado não foi encontrado.",
+        "405" => "O método de requisição utilizado não é permitido para este recurso.",
+        "422" => "Ocorreram erros de validação. Por favor, verifique os dados enviados.",
+        "500" => "Ocorreu um erro interno no servidor.",
+        "503" => "O serviço não está disponível no momento. Tente novamente mais tarde."
+    ],
 ];
