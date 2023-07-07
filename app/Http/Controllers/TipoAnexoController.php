c4e2aecf5c74e91b4316f84e55f1dda97937b828
natanaeldeveloper <712020:55cb029b-7bd2-4758-8b1b-b71541144f89@bots.bitbucket.org>
2023-07-06 23:59:42 -0300

    feat: adicionado controller do artefato de tipos de anexo

--- app/Http/Controllers/TipoAnexoController.php
+++ app/Http/Controllers/TipoAnexoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTipoAnexoRequest;
+use App\Http\Requests\UpdateTipoAnexoRequest;
 use App\Models\TipoAnexo;
 use Illuminate\Http\Request;
 
@@ -12,15 +14,23 @@ class TipoAnexoController extends Controller
      */
     public function index()
     {
-        //
+        $tipoAnexoList = TipoAnexo::orderBy('nome')->paginate(10);
+
+        return response()->json($tipoAnexoList);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTipoAnexoRequest $request)
     {
-        //
+        $tipoAnexo = TipoAnexo::create($request->all());
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.created.success'),
+            'data'      => $tipoAnexo
+        ], 201);
     }
 
     /**
@@ -28,15 +38,23 @@ class TipoAnexoController extends Controller
      */
     public function show(TipoAnexo $tipoAnexo)
     {
-        //
+        return response()->json([
+            'data' => $tipoAnexo,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, TipoAnexo $tipoAnexo)
+    public function update(UpdateTipoAnexoRequest $request, TipoAnexo $tipoAnexo)
     {
-        //
+        $tipoAnexo->update($request->all());
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.updated.success'),
+            'data'      => $tipoAnexo
+        ]);
     }
 
     /**
@@ -44,6 +62,12 @@ class TipoAnexoController extends Controller
      */
     public function destroy(TipoAnexo $tipoAnexo)
     {
-        //
+        $tipoAnexo->delete();
+
+        return response()->json([
+            'status'    => 'success',
+            'message'   => __('messages.deleted.success'),
+            'id'        => $tipoAnexo->id,
+        ]);
     }
 }
