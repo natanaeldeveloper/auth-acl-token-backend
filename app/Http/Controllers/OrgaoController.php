eb08cfb8d4c2f6ec6d79aebe9e3859c095b03b28
natanaeldeveloper <712020:55cb029b-7bd2-4758-8b1b-b71541144f89@bots.bitbucket.org>
2023-07-10 19:20:24 -0300

    feat: adicionado lógica do crud de 'orgao'

--- app/Http/Controllers/OrgaoController.php
+++ app/Http/Controllers/OrgaoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOrgaoRequest;
+use App\Http\Requests\UpdateOrgaoRequest;
+use App\Http\Resources\OrgaoCollection;
+use App\Http\Resources\OrgaoResource;
 use App\Models\Orgao;
 use Illuminate\Http\Request;
 
@@ -12,15 +16,34 @@ class OrgaoController extends Controller
      */
     public function index()
     {
-        //
+        $orgaos = Orgao::with([
+            'orgaoResponsavel',
+            'tipoOrgao',
+        ])
+            ->orderBy('created_at', 'DESC')
+            ->paginate(10);
+
+        $data = new OrgaoCollection($orgaos);
+
+        return response()->json($data);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreOrgaoRequest $request)
     {
-        //
+        $orgao = Orgao::create($request->all());
+
+        $orgao->tiposAnexo()->attach($request->tipos_anexo_id);
+
+        $data = new OrgaoResource($orgao);
+
+        return response()->json([
+            'status' => 'success',
+            'messages' => __('messages.created.success'),
+            'data' => $data,
+        ], 201);
     }
 
     /**
@@ -28,15 +51,29 @@ class OrgaoController extends Controller
      */
     public function show(Orgao $orgao)
     {
-        //
+        $data = new OrgaoResource($orgao);
+
+        return response()->json([
+            'data' => $data
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Orgao $orgao)
+    public function update(UpdateOrgaoRequest $request, Orgao $orgao)
     {
-        //
+        $orgao->update($request->all());
+
+        $orgao->tiposAnexo()->sync($request->tipos_anexo_id);
+
+        $data = new OrgaoResource($orgao);
+
+        return response()->json([
+            'status' => 'success',
+            'messages' => __('messages.updated.success'),
+            'data' => $data,
+        ]);
     }
 
     /**
@@ -44,6 +81,13 @@ class OrgaoController extends Controller
      */
     public function destroy(Orgao $orgao)
     {
-        //
+        $orgao->tiposAnexo()->sync([]);
+        $orgao->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'messages' => __('messages.deleted.success'),
+            'id' => $orgao->id,
+        ]);
     }
 }
