4fdaf205d3208a37ec16a32c930a13526b3b2ad9
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-08-04 14:40:50 -0300

    feat: adicionada migration de anexos

---
+++ database/migrations/2023_08_01_210420_create_anexos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anexos', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('mime_type')->nullable();
+            $table->foreignId('processo_id');
+            $table->foreignId('tipo_anexo_id');
+            $table->foreignId('editor_id');
+            $table->string('descricao');
+            $table->text('conteudo')->nullable();
+            $table->boolean('por_arquivo');
+            $table->timestamps();
+
+            $table->foreign('processo_id')
+                ->references('id')
+                ->on('processos');
+
+            $table->foreign('tipo_anexo_id')
+                ->references('id')
+                ->on('tipos_anexos');
+
+            $table->foreign('editor_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anexos');
+    }
+};
