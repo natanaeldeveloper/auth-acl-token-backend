dda4ae3eafae857941bc3ef0e00ceb37bc2b6305
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-08-04 16:09:52 -0300

    feat: adicionado hateoas de anexo

---
+++ app/Hateoas/AnexoHateoas.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Hateoas;
+
+use App\Models\Anexo;
+use GDebrauwer\Hateoas\Traits\CreatesLinks;
+
+class AnexoHateoas
+{
+    use CreatesLinks;
+
+    public function self(Anexo $anexo)
+    {
+        return $this->link('processo.anexo.show', ['processo' => $anexo->processo_id, 'anexo' => $anexo->id]);
+    }
+
+    public function update(Anexo $anexo)
+    {
+        return $this->link('processo.anexo.update', ['processo' => $anexo->processo_id, 'anexo' => $anexo->id]);
+    }
+
+    public function remove(Anexo $anexo)
+    {
+        return $this->link('processo.anexo.destroy', ['processo' => $anexo->processo_id, 'anexo' => $anexo->id]);
+    }
+
+    public function assinar(Anexo $anexo)
+    {
+        return $this->link('processo.anexo.destroy', ['processo' => $anexo->processo_id, 'anexo' => $anexo->id]);
+    }
+
+    public function download(Anexo $anexo)
+    {
+        return $this->link('processo.anexo.destroy', ['processo' => $anexo->processo_id, 'anexo' => $anexo->id]);
+    }
+}
